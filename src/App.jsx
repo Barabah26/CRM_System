67c21afdcb508ecf80f302e66578feb0a9824671
Barabah26 <pavlobarabah@gmail.com>
2023-09-20 19:22:24 +0300

    Зробив сторінку вхід

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { Routes } from 'react-router-dom'; // Додайте Routes та Route
-
 import Login from './components/Login';
 import Home from './components/Home';
 import DataTable from './components/DataTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import DataTable from './components/DataTable';
+import './components/FormStyle.css'
 
 function App() {
-
-
   
   return (
     <Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,80 +1,63 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './FormStyle.css'
 
 
-function Login() {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
+// function Login() {
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: '',
+//   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Отправка даних на сервер для входу користувача
-    console.log('Відправлені дані:', formData);
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Отправка даних на сервер для входу користувача
+//     console.log('Відправлені дані:', formData);
+//   };
 
-  return (
-   <>
-   <div className='container'>
-   <NavBar/>
-
-   
-   <div className="container d-flex justify-content-center">
-      
-      <div className="FormStyle.container">
-        <h2>Вхід</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <div/>
-            <input
-              type="email"
-              className="FormStyle.form-control-sm"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Пароль</label>
-            <div/>
-            <input
-              type="password"
-              className="FormStyle.form-control-sm"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            /> 
-            <div/>
-          </div>
-          <div className='mt-2'>
-            <button type="submit" className="btn btn-primary">
-              Увійти
-            </button>
-          </div>
+  function Login() {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Додайте код для обробки даних форми
+    };
+  
+    return (
+      <div className='body'>
+        <form onSubmit={handleSubmit} className='form-container form'>
+          <h3>Авторизація користувача</h3>
+          <TextField
+            label="Ім'я користувача"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Пароль"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            type="password"
+            required
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Увійти
+          </Button>
         </form>
       </div>
-    </div>
-   </div>
-   
-   
-   </>
-    
   );
 }
+   
 
 export default Login;
