e4577c11290d4a66887d3806641c123dc7ed8a6a
Barabah26 <pavlobarabah@gmail.com>
2023-09-27 21:05:39 +0300

    Зробив авторизацію користувача

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { Routes } from 'react-router-dom'; // Додайте Routes та Route
 import Login from './components/Login';
-import Home from './components/Home';
 import DataTable from './components/DataTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import DataTable from './components/DataTable';
@@ -13,7 +12,6 @@ function App() {
   return (
     <Router>
       <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/dataTable" element={<DataTable />} />
       </Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,63 +1,53 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './NavBar';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import './FormStyle.css'
+import './FormStyle.css';
 
+// Замість статичних даних, ви можете підключити більш складний механізм аутентифікації
+const user = {
+  login: 'myLogin',
+  password: 'myPassword',
+};
 
-// function Login() {
-//   const [formData, setFormData] = useState({
-//     email: '',
-//     password: '',
-//   });
+function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const enteredLogin = e.target.login.value;
+    const enteredPassword = e.target.password.value;
 
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
+    if (enteredLogin === user.login && enteredPassword === user.password) {
+      // Перекидаємо користувача на сторінку DataTable
+      window.location.href = '/DataTable';
+    } else {
+      alert('Неправильний логін або пароль');
+    }
+  };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Отправка даних на сервер для входу користувача
-//     console.log('Відправлені дані:', formData);
-//   };
-
-  function Login() {
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Додайте код для обробки даних форми
-    };
-  
-    return (
-      <div className='body'>
-        <form onSubmit={handleSubmit} className='form-container form'>
-          <h3>Авторизація користувача</h3>
-          <TextField
-            label="Ім'я користувача"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            required
-          />
-          <TextField
-            label="Пароль"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            type="password"
-            required
-          />
-          <Button type="submit" variant="contained" color="primary">
-            Увійти
-          </Button>
-        </form>
-      </div>
+  return (
+    <form onSubmit={handleSubmit} className='form-container form'>
+      <h3>Авторизація користувача</h3>
+      <TextField
+        name="login" // Додайте ім'я поля для логіну
+        label="Ім'я користувача"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        required
+      />
+      <TextField
+        name="password" // Додайте ім'я поля для паролю
+        label="Пароль"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        type="password"
+        required
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Увійти
+      </Button>
+    </form>
   );
 }
-   
 
 export default Login;
